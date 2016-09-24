79c26c9b86cb7bb2ef5eb7677b14233b8344bea2
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-09-24 12:21:02 -0500

    subiendo archivos

---
+++ README.md
@@ -0,0 +1,17 @@
+#####TRELLO
+
+##Versión 0.0.1
+1.- Un elemento en el HTML con el mensaje "Añadir una lista", que al dar click muestre un input y un botón (formulario) para que el usuario ingrese el nombre de la lista.  
+
+##Versión 0.0.2
+1.-Mostrar en el HTML, el texto ingresado al dar click en el botón de "Guardar" del formulario (como si fuera título de la lista).  
+2.-Debajo del título, mostrar el mensaje clickeable de "Añadir una tarjea".  
+
+##VERSIÓN 0.0.3
+1.- Una vez agregada la lista, mostrar el mensaje clickeable de "Añadir una lista" al lado de la lista agregada.  
+
+##VERSIÓN 0.0.4
+1.-Al dar click en "Añadir una lista", asegurarse que el input del formulario tenga el focus para poder escribir directamente el nombre de la lista.  
+2.- Dar click al mensaje "Añadir una tarjeta" y mostrar e formulario para agregar la tarjeta (Nota: El ingreso de texto es mediante un textarea).  
+
+![imagen](http://i67.tinypic.com/qz1yk8.png)
---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function() {
+	var wall = document.getElementById("wall");
+	var nombre = document.getElementById("nombre");
+	var cuadro = document.getElementById("cuadro")
+	cuadro.style.display = 'none'
+	wall.addEventListener("click", function() {
+		cuadro.style.display = 'none'; 
+		swal({   
+			title: "Bienvenido!",   
+			text: "Cual es tu nombre?",   
+			type: "input",   
+			showCancelButton: true,   
+			closeOnConfirm: false,  
+			animation: "slide-from-top",   
+			inputPlaceholder: "mi nombre es..." 
+		}, 
+		function(inputValue){ 
+
+			if (inputValue === false) 
+				return false;      
+			if (inputValue === "") { 
+		    	swal.showInputError("Ups! Necesito saber tu nombre!");     
+		    	return false   }      
+		    swal("Bien!", "Hola " + inputValue, "success"); 
+			nombre.innerHTML = "Bienvenido " + inputValue;
+			cuadro.style.display = 'block';
+		});
+		wall.style.display = 'none';
+
+	});
+});
+
