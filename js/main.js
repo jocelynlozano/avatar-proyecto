581c44a6ba0cde1a68b816f9d8ffc97ce213ecf8
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-09-25 22:24:06 -0500

    subiendo cambios de segunda pagina

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
 window.addEventListener("load", function() {
-	var wall = document.getElementById("wall");
-	var nombre = document.getElementById("nombre");
-	var cuadro = document.getElementById("cuadro");
-	var cuadro1 = document.getElementById("cuadro1");
-	var	cuadro2 = document.getElementById("cuadro2");
-	var vestido = document.getElementById("vestido")
-	cuadro.style.display = 'none'
-	wall.addEventListener("click", function() {
-		cuadro.style.display = 'none'; 
-		swal({   
-			title: "Bienvenido!",   
-			text: "Cual es tu nombre?",   
-			type: "input",   
-			showCancelButton: true,   
-			closeOnConfirm: false,  
-			animation: "slide-from-top",   
-			inputPlaceholder: "mi nombre es..." 
-		}, 
-		function(inputValue){ 
-			var nombre = document.getElementById("nombre");
-			if (inputValue === false) 
-				return false;      
-			if (inputValue === "") { 
-		    	swal.showInputError("Ups! Necesito saber tu nombre!");     
-		    	return false   }      
-		    swal("Bien!", "Que comienze el juego " + inputValue, "success"); 
-			nombre.innerHTML = "Bienvenido " + inputValue;
-			nombre.classList.add("colorsito");
-			cuadro.style.display = 'block';
-			location.href = "#abajo";
-		});
-		wall.style.display = 'none';
-	});
+    var wall = document.getElementById("wall");
+    var nombre = document.getElementById("nombre");
+    var cuadro = document.getElementById("cuadro");
+    var cuadro1 = document.getElementById("cuadro1");
+    var cuadro2 = document.getElementById("cuadro2");
+    var vestido = document.getElementById("vestido")
+    cuadro.style.display = 'none'
+    wall.addEventListener("click", function() {
+        cuadro.style.display = 'none'; 
+        swal({   
+            title: "Bienvenido!",   
+            text: "Cual es tu nombre?",   
+            type: "input",   
+            showCancelButton: true,   
+            closeOnConfirm: false,  
+            animation: "slide-from-top",   
+            inputPlaceholder: "mi nombre es..." 
+        }, 
+        function(inputValue){ 
+            var nombre = document.getElementById("nombre");
+            if (inputValue === false) 
+                return false;      
+            if (inputValue === "") { 
+                swal.showInputError("Ups! Necesito saber tu nombre!");     
+                return false   }      
+            swal("Bien!", "Que comienze el juego " + inputValue, "success"); 
+            nombre.innerHTML = "Bienvenido " + inputValue;
+            nombre.classList.add("colorsito");
+            cuadro.style.display = 'block';
+            location.href = "#abajo";
+        });
+        wall.style.display = 'none';
+    });
 
-	cuadro1.addEventListener("dragstart", function(event){
-		event.dataTransfer.setData("Text", event.target.id);
-	});
+    cuadro1.addEventListener("dragstart", function(event){
+        event.dataTransfer.setData("Text", event.target.id);
+    });
 
-	cuadro2.addEventListener("dragover", function(event){
-			event.preventDefault();
-	})
-	cuadro2.addEventListener("drop", function(event){
-		event.preventDefault();
-		var data= event.dataTransfer.getData("text");
-		event.target.appendChild(document.getElementById(data));
-		});
-	function carga(){
+    cuadro2.addEventListener("dragover", function(event){
+            event.preventDefault();
+    })
+    cuadro2.addEventListener("drop", function(event){
+        event.preventDefault();
+        var data= event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(data));
+        });
+    function carga(){
     posicion=0;
   vestido.addEventListener("mousedown",function(event){
   comienzoMovimiento(event, id);
@@ -61,7 +61,7 @@ function evitaEventos(event){
     event.preventDefault();
 } 
 function comienzoMovimiento(event, id){
-    elMovimiento=document.getElementById(id);
+    elMovimiento=document.getElementById("id");
 
         cursorComienzoX=event.clientX+window.scrollX;
         cursorComienzoY=event.clientY+window.scrollY;
