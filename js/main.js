71a1ab8a8e10b3466ab831bd025db3cff233a64a
Sandy Ciquero <sandy.ciqueroch@gmail.com>
2016-09-26 08:01:02 -0500

    agrendo cambios en la ropa

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ window.addEventListener("load", function() {
     var cuadro = document.getElementById("cuadro");
     var cuadro1 = document.getElementById("cuadro1");
     var cuadro2 = document.getElementById("cuadro2");
-    var vestido = document.getElementById("vestido")
+    var vestido = document.getElementById("vestido");
+    var doll = document.getElementById("dragtarget");
+    var ropa1 =document.getElementById("ropa1");
     cuadro.style.display = 'none'
     wall.addEventListener("click", function() {
         cuadro.style.display = 'none'; 
@@ -32,11 +34,10 @@ window.addEventListener("load", function() {
         });
         wall.style.display = 'none';
     });
-
     cuadro1.addEventListener("dragstart", function(event){
+        event.dataTransfer.effecAllowed = 'move';
         event.dataTransfer.setData("Text", event.target.id);
     });
-
     cuadro2.addEventListener("dragover", function(event){
             event.preventDefault();
     })
@@ -45,52 +46,68 @@ window.addEventListener("load", function() {
         var data= event.dataTransfer.getData("text");
         event.target.appendChild(document.getElementById(data));
         });
+    vestido.addEventListener("dragstart", function(event){
+        event.dataTransfer.setData("Text", event.target.id);
+        ropa1.addEventListener("dragstart", function(event){
+        event.dataTransfer.setData("Text", event.target.id);
+    });
+    
+    doll.addEventListener("dragover", function(event){
+            event.preventDefault();
+    });
+    doll.addEventListener("drop", function(event){
+        event.preventDefault();
+        var data= event.dataTransfer.getData("text");
+        event.target.insertBefore(document.getElementById(data));
+        });
+
+    
     function carga(){
-    posicion=0;
-  vestido.addEventListener("mousedown",function(event){
-  comienzoMovimiento(event, id);
-})
+        posicion=0;
+        vestido.addEventListener("mousedown",function(event){
+        comienzoMovimiento(event, id);
+        })
 
-vestido.addEventListener("mouseover",function(event){
-  this.style.cursor='move'
-})
+        vestido.addEventListener("mouseover",function(event){
+        this.style.cursor='move'
+        })
+    }
+    });
 
-}
-function evitaEventos(event){
-    // Funcion que evita que se ejecuten eventos adicionales
-    event.preventDefault();
-} 
-function comienzoMovimiento(event, id){
-    elMovimiento=document.getElementById("id");
+    // function evitaEventos(event){
+    // // Funcion que evita que se ejecuten eventos adicionales
+    // event.preventDefault();
+    // } 
+    // function comienzoMovimiento(event, id){
+    // elMovimiento=document.getElementById("id");
 
-        cursorComienzoX=event.clientX+window.scrollX;
-        cursorComienzoY=event.clientY+window.scrollY;
+    //     cursorComienzoX=event.clientX+window.scrollX;
+    //     cursorComienzoY=event.clientY+window.scrollY;
        
-        document.addEventListener("mousemove", enMovimiento, true);
-        document.addEventListener("mouseup", finMovimiento, true);
+    //     vestido.addEventListener("mousemove", enMovimiento, true);
+    //     vestido.addEventListener("mouseup", finMovimiento, true);
    
-    elComienzoX=parseInt(elMovimiento.style.left);
-    elComienzoY=parseInt(elMovimiento.style.top);
-    // Actualizo el posicion del elemento
-    elMovimiento.style.zIndex=++posicion;
+    // elComienzoX=parseInt(elMovimiento.style.left);
+    // elComienzoY=parseInt(elMovimiento.style.top);
+    // // Actualizo el posicion del elemento
+    // elMovimiento.style.zIndex=++posicion;
 
-    evitaEventos(event);
-}
-function enMovimiento(event){ 
-    var xActual, yActual;
-        xActual=event.clientX+window.scrollX;
-        yActual=event.clientY+window.scrollY;
+    // evitaEventos(event);
+    // }
+    // function enMovimiento(event){ 
+    // var xActual, yActual;
+    //     xActual=event.clientX+window.scrollX;
+    //     yActual=event.clientY+window.scrollY;
    
-    elMovimiento.style.left=(elComienzoX+xActual-cursorComienzoX)+"px";
-    elMovimiento.style.top=(elComienzoY+yActual-cursorComienzoY)+"px";
+    // elMovimiento.style.left=(elComienzoX+xActual-cursorComienzoX)+"px";
+    // elMovimiento.style.top=(elComienzoY+yActual-cursorComienzoY)+"px";
  
-    evitaEventos(event);
-}
-function finMovimiento(event){
-        document.removeEventListener("mousemove", enMovimiento, true);
-        document.removeEventListener("mouseup", finMovimiento, true);
-}
+    // evitaEventos(event);
+    // }
+    // function finMovimiento(event){
+    //     vestido.removeEventListener("mousemove", enMovimiento, true);
+    //     vestido.removeEventListener("mouseup", finMovimiento, true);
+    // }   
 
 });
-
-
+    
